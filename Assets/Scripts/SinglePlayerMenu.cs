e997b2d460991d17c77e932b7c5b7af4337bc97d
Matias Rietig <rietig.m@googlemail.com>
2020-12-06 23:23:13 +0100

    added first menu

---
+++ Assets/Scripts/SinglePlayerMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SinglePlayerMenu : MonoBehaviour
+{
+    public void PlayRPS() {
+        SceneManager.LoadScene("RockPaperScissors");
+    }
+
+    public void PlayDrawing() {
+        SceneManager.LoadScene("Drawing");
+    }
+}
